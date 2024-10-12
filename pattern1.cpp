1f24e8816d9566385d82be5036984db014da0999
Md Jehid Alam <146694343+mdjehidalam@users.noreply.github.com>
2024-10-12 11:32:59 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+  int n , m; 
+  cout<<"Enter the row No: "<<flush; 
+  cin>>n; 
+ cout<<endl; 
+  cout<<"Enter the col No : "<<flush; 
+  cin>>m; 
+  cout<<endl; 
+  for(int i=0; i<n; i++)
+  {
+    for(int j =0; j<m; j++)
+    {
+      cout<<" * ";
+    }
+    cout<<endl; 
+  }
+  
+}
