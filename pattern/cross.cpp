7440d296b03589b00696127378b57db09e8ae6c9
Md Jehid Alam <146694343+mdjehidalam@users.noreply.github.com>
2024-10-12 12:57:50 +0530

    Create cross.cpp

---
+++ pattern/cross.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+    int n; 
+    int m; 
+    cout<<"Enter the n: "<<flush;
+    cin>>n; 
+    for(int i=0; i<n; i++)
+    {
+         
+        for(int j=0; j<n; j++)
+        {
+             if(i==j || i+j ==n-1)
+             {
+                cout<<"*";
+             }else
+             cout<<" ";
+        }
+        cout<<endl; 
+    }
+}
