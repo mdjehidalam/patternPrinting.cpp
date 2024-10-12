242c655673defdaff41431fad228c1853e62a447
Md Jehid Alam <146694343+mdjehidalam@users.noreply.github.com>
2024-10-12 11:56:43 +0530

    Create TriangleOldNumber.cpp

---
+++ TriangleOldNumber.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+    int n; 
+    int m; 
+    cout<<"Enter the n: "<<flush;
+    cin>>n; 
+    for(int i=0; i<n; i++)
+    {
+         int a = 1; 
+        for(int j=0; j<i; j++)
+        {
+            
+             cout<<a; 
+             a+=2; 
+        }
+        cout<<endl; 
+    }
+}
